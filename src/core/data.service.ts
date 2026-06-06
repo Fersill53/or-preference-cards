64b1bbf8fae6f2bf4adcad03cf3fb160a9b270db
Fernando Siller <fernando@Fernandos-MacBook-Pro.local>
2026-06-06 18:36:56 -0500

    added to data.service.ts to fix error

--- src/core/data.service.ts
+++ src/core/data.service.ts
@@ -144,6 +144,7 @@ export class DataService {
           procedureName: 'Total Knee Arthroplasty',
           notes: 'Tourniquet. Cemented. Keep extra #10 blades.',
           instruments: ['Basic Ortho', 'TKA System', 'Pulsavac', 'Cement Bowl + Gun'],
+          onMayo: [''],
           positioning: 'Supine, bump under hip, well leg padded.',
           prep: 'Chloraprep full leg to high thigh.',
           antibiotics: 'Ancef 2g (or per allergy).',
